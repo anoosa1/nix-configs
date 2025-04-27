ab25845a3a932488f37f49b8bd91ec4d0560918f
Anas <anas@asherif.xyz>
2025-04-27 02:23:28 -0400

    Updated config

--- home-manager/river/river.nix
+++ home-manager/river/river.nix
@@ -16,7 +16,7 @@
       border-width = 2;
       set-repeat = "50 300";
       xcursor-theme = "Bibata-Modern-Ice 24";
-      output-layout = "rivertile";
+      output-layout = "rivertile -view-padding 3 -outer-padding 3";
 
       declare-mode = [
         "locked"
