2baa54438c48c7c271c6b45759d2f13aceb7462c
Anas <anas@asherif.xyz>
2025-06-19 06:17:54 -0400

    Updated services.

--- services/soft-serve.nix
+++ services/soft-serve.nix
@@ -6,13 +6,18 @@
       settings = {
         name = "asherif.xyz";
         log_format = "text";
+        stats.listen_addr = ":23233";
+
         ssh = {
           listen_addr = ":23231";
           public_url = "ssh://asherif.xyz:23231";
           max_timeout = 30;
           idle_timeout = 120;
         };
-        stats.listen_addr = ":23233";
+
+        initial_admin_keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhK2hN/aKRr12GA6dklSQbL+jG5iQ9OuvXzprvzfGc8"
+        ];
       };
     };
   };
