ba1839850d8422148850fc72fd3279422659d9ef
anas <anas@asherif.xyz>
2026-01-06 21:25:25 -0500

    Updated config.

--- users/anas/services/default.nix
+++ users/anas/services/default.nix
@@ -2,14 +2,7 @@
   imports = [
     ./dunst.nix
     ./gpg-agent.nix
-    ./mpd.nix
     ./pass-secret-service.nix
     ./wpaperd.nix
   ];
-
-  services = {
-    playerctld = {
-      enable = true;
-    };
-  };
 }
