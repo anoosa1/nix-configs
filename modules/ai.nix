27316d1993f9350429d3f143566ecb36d85c10f0
Anas <anas@asherif.xyz>
2026-05-30 02:05:35 -0400

    Update.

--- modules/ai.nix
+++ modules/ai.nix
@@ -8,25 +8,27 @@
     services = {
       open-webui = {
         enable = true;
-        host = "127.0.0.1";
+        host = "0.0.0.0";
         port = 3080;
         openFirewall = false;
         stateDir = "/var/lib/open-webui";
         environmentFile = "/run/secrets/open-webui";
 
         environment = {
-          WEBUI_URL = "https://chat.asherif.xyz";
+          DEFAULT_USER_ROLE = "user";
+          ENABLE_LOGIN_FORM = "false";
+          ENABLE_OAUTH_ROLE_MANAGEMENT = "true";
           ENABLE_OAUTH_SIGNUP = "true";
+          OAUTH_ADMIN_ROLES = "admin";
+          OAUTH_CODE_CHALLENGE_METHOD = "S256";
           OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "true";
           OAUTH_PROVIDER_NAME = "Authelia";
-          OAUTH_SCOPES = "openid profile email groups";
-          OAUTH_CODE_CHALLENGE_METHOD = "S256";
-          OPENID_PROVIDER_URL = "https://auth.asherif.xyz/.well-known/openid-configuration";
-          ENABLE_OAUTH_ROLE_MANAGEMENT = "true";
-          OAUTH_ADMIN_ROLES = "admin";
           OAUTH_ROLES_CLAIM = "groups";
-          OPENAI_API_BASE_URL = "https://api.deepseek.com";
-          RAG_OPENAI_API_BASE_URL = "http://127.0.0.1:8888/v1";
+          OAUTH_SCOPES = "openid profile email groups";
+          OAUTH_USERNAME_CLAIM = "preferred_username";
+          OPENID_PROVIDER_URL = "https://auth.asherif.xyz/.well-known/openid-configuration";
+          WEBUI_NAME = "Soosa";
+          WEBUI_URL = "https://chat.asherif.xyz";
         };
       };
 
@@ -40,6 +42,7 @@
             locations."/" = {
               proxyPass = "http://127.0.0.1:3080";
               proxyWebsockets = true;
+
               extraConfig = ''
                 proxy_set_header Host $host;
                 proxy_set_header X-Real-IP $remote_addr;
